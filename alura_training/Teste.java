76ae1e7355a9bf407b25395d38e481901b74864c
edneyRoldao <edneyroldao@gmail.com>
2017-09-21 15:00:54 -0300

    code

--- alura_training/Teste.java
+++ alura_training/Teste.java
@@ -2,16 +2,11 @@ import java.util.*;
 
 class Teste {
 	public static void main(String[] args) {
-		
-		List<String> lista = new ArrayList<>();
-		lista.add("edney");
-		lista.add("giselle");
-		lista.add("nadine");
-		lista.add("marjorie");
 
-		lista.sort(s -> Comparator.comparing(s.length()));
 
-		lista.forEach(System.out::println);
+		int i = 0;
+		do; while(i++ < 10);
+
 
 	}
 }
