314bb830df9af40fd753bba9e0f46dbb4017c32d
edneyRoldao <edneyroldao@gmail.com>
2017-10-04 05:27:11 -0300

    sss

--- alura_training/Teste.java
+++ alura_training/Teste.java
@@ -2,10 +2,25 @@
 // Given the code fragment:
 class Test {
 	public static void main(String[] args) {
-		int num = 5;
-		do {
-			System.out.print(num-- + " ");
-		} while(num == 0);
+		String[][] chs = new String[2][];
+		chs[0] = new String[2];
+		chs[1] = new String[5];
+		int i = 97;
+
+		for(int a = 0; a < chs.length; a++) {
+			for(int b = 0; b < chs.length; b++) {
+				chs[a][b] = "" + i;
+				i++;
+			}
+		}
+
+		for(String[] ca : chs) {
+			for(String c : ca) {
+				System.out.print(c + " ");
+			}
+			System.out.println();
+		}
+
 	}
 }
 
