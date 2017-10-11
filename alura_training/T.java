ef762df2c5ecaeac88cd4b7cbff454f10bab2456
edneyRoldao <edneyroldao@gmail.com>
2017-10-11 12:05:40 -0300

    final code

--- alura_training/T.java
+++ alura_training/T.java
@@ -1,15 +1,25 @@
-class t {
-	public static void main(String[] args) {
-		Boolean[] bool = new Boolean[2];
-
-		bool[0] =  new Boolean(Boolean.parseBoolean("true"));
-		bool[1] = new Boolean(null);
-
-		System.out.println(bool[0] + " " + bool[1]);
-	}
+class Employee {
+	int salary;
 }
 
+class Manager extends Employee {
+	int budget;
+}
 
+class Director extends Manager {
+	int stockOptions;
+}
+
+class Test {
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		Manager manager = new Manager();
+		Director director = new Director();
+
+		employee.budget = 200_000;
+		manager.stockOption = 500; 
+	}
+}
 
 
 
